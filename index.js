29e6a3046b31fc35af21fd077c31a8d750592849
Anthony Dole <anthony.dole.pro@mail.com>
2023-03-15 11:36:41 +0100

    try headless option

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const main = async () => {
   const browser = await puppeteer.launch({
     executablePath: "/usr/bin/google-chrome",
     ignoreDefaultArgs: ["--disable-extensions"],
-    headless: true,
+    headless: false,
     args: ["--no-sandbox", "--disabled-setupid-sandbox"],
   });
   const page = await browser.newPage();
